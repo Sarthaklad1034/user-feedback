9fc49221efb2e05139c0fee579439dd5e4678faf
Sarthak <Sarthaklad1034@gmail.com>
2024-10-02 01:32:06 +0530

    Update App.js with data written into alert

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import FeedbackForm from './components/FeedbackForm/FeedbackForm';
 function App() {
     const handleSubmit = (formData) => {
         // Here you would typically send the data to a server
-        console.log('Form submitted:', formData);
-        alert('Thank you for your feedback!');
+        // Destructure the formData object to get the values
+        const { name, comment } = formData;
+
+        // Display the values in the alert
+        alert(`Thank you for your feedback!\nName: ${name}\nComment: ${comment}`);
     };
 
     return ( <
         div className = "App" >
         <
         FeedbackForm onSubmit = { handleSubmit }
-        /> <
-        /div>
+        /> < /
+        div >
     );
 }
 
